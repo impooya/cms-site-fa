ff93e0a55f82b11a6368f9fa257702ae33049720
impooya <samarkarpooya@gmail.com>
2024-08-27 15:37:39 +0330

    config css style

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,7 +1,7 @@
 export default function Root() {
   return (
     <>
-      <h1 className="flex text-blue-300">Root</h1>
+      <h1 className="flex text-indigo-700 text-3xl">سلام</h1>
     </>
   );
 }
