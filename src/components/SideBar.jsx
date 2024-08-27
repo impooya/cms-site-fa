cc116a1474367d62d841aa164146d2367d45a5d2
impooya <samarkarpooya@gmail.com>
2024-08-27 21:02:51 +0330

    add SideBar components  and styled SideBar components

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,61 @@
+export default function SideBar() {
+  return (
+    <>
+      <nav className="flex flex-col divide-y-2 divide-violet-400 bg-indigo-700 h-screen fixed inset-0 w-72 ">
+        <h1 className="text-[22.4px] inline-block text-white p-4 text-center">
+          به داشبورد خوش آمدید
+        </h1>
+        <ul className="flex flex-col items-start justify-center pt-5 gap-6">
+          <li className="w-full  h-16 flex justify-start items-center hover:bg-blue-700 transition-all ">
+            <a
+              className="w-auto h-full inline-flex items-center text-white text-2xl  pr-4 active:bg-blue-700"
+              href="#"
+            >
+              صفحه اصلی
+            </a>
+          </li>
+          <li className="w-full h-16 flex justify-start items-center hover:bg-violet-900 transition-all ">
+            <a
+              className="h-full w-auto text-white text-2xl inline-flex items-center pr-4 active:bg-violet-900"
+              href="#"
+            >
+              محصولات
+            </a>
+          </li>
+          <li className="w-full h-16 flex justify-start items-center hover:bg-violet-900 transition-all ">
+            <a
+              className="h-full w-auto text-white text-2xl inline-flex items-center pr-4 active:bg-violet-900"
+              href="#"
+            >
+              کامنت ها
+            </a>
+          </li>
+          <li className="w-full h-16 flex justify-start items-center hover:bg-violet-900 transition-all ">
+            <a
+              className="h-full w-auto text-white text-2xl inline-flex items-center pr-4 active:bg-violet-900"
+              href="#"
+            >
+              کاربران
+            </a>
+          </li>
+          <li className="w-full h-16 flex justify-start items-center hover:bg-violet-900 transition-all ">
+            <a
+              className="h-full w-auto text-white text-2xl inline-flex items-center pr-4 active:bg-violet-900"
+              href="#"
+            >
+              سفارشات
+            </a>
+          </li>
+          <li className="w-full h-16 flex justify-start items-center hover:bg-violet-900 transition-all ">
+            <a
+              className="h-full w-auto text-white text-2xl inline-flex items-center pr-4 active:bg-violet-900"
+              href="#"
+            >
+              تخفیف ها
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,7 +1,8 @@
+import SideBar from "../components/SideBar";
 export default function Root() {
   return (
     <>
-      <h1 className="flex text-indigo-700 text-3xl">سلام</h1>
+      <SideBar />
     </>
   );
 }
