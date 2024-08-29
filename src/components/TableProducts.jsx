49dac2336eab64fe0f02b79f1192d9dbcd2b356e
impooya <samarkarpooya@gmail.com>
2024-08-29 13:00:30 +0330

    fix some bugs in TableProducts

--- src/components/TableProducts.jsx
+++ src/components/TableProducts.jsx
@@ -1,9 +1,9 @@
 export default function TableProducts() {
   return (
     <>
-      <section className="w-full bg-white mt-4 rounded-2xl shadow-2xl pt-4 pr-8">
-        <table className="w-full">
-          <thead className="h-20 text-right text-2xl">
+      <section className="w-full bg-white mt-4 rounded-2xl shadow-2xl pt-4 ">
+        <table className="w-full divide-y-2 divide-zinc-700">
+          <thead className="h-20 text-center text-2xl">
             <tr>
               <th>عکس</th>
               <th>اسم</th>
@@ -11,7 +11,7 @@ export default function TableProducts() {
               <th>موجودی</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="text-center divide-y-2">
             <tr className="child:w-56 child:text-xl">
               <td>
                 <img src="../image/headphone.jpeg" className="size-64" />
@@ -25,7 +25,7 @@ export default function TableProducts() {
                 <button type="button">ویرایش</button>
               </td>
             </tr>
-            <tr className="child:w-56 child:text-lg">
+            <tr className="child:w-56 child:text-xl">
               <td>
                 <img src="../image/head.jpeg" className="size-64" />
               </td>
@@ -38,7 +38,7 @@ export default function TableProducts() {
                 <button type="button">ویرایش</button>
               </td>
             </tr>
-            <tr className="child:w-56 child:text-lg">
+            <tr className="child:w-56 child:text-xl">
               <td>
                 <img src="../image/charger.jpeg" className="size-64" />
               </td>
