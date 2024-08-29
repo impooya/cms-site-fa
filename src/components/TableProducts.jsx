ea50778854688fc8e3e5225cbff766948a8b0056
impooya <samarkarpooya@gmail.com>
2024-08-29 12:26:16 +0330

    style TabelProducts template

--- src/components/TableProducts.jsx
+++ src/components/TableProducts.jsx
@@ -1,9 +1,9 @@
 export default function TableProducts() {
   return (
     <>
-      <section>
-        <table>
-          <thead>
+      <section className="w-full bg-white mt-4 rounded-2xl shadow-2xl pt-4 pr-8">
+        <table className="w-full">
+          <thead className="h-20 text-right text-2xl">
             <tr>
               <th>عکس</th>
               <th>اسم</th>
@@ -12,43 +12,43 @@ export default function TableProducts() {
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr className="child:w-56 child:text-xl">
               <td>
-                <img src="../image/headphone.jpeg"></img>
+                <img src="../image/headphone.jpeg" className="size-64" />
               </td>
               <td>هنذفری بلوتوثی</td>
               <td>200000 تومان</td>
               <td>134</td>
-              <td>
-                <button>جزِییات</button>
-                <button>حذف</button>
-                <button>ویرایش</button>
+              <td className="child:ml-5 child:bg-blue-700 child:w-20 child:h-14 child:rounded-2xl child:text-white">
+                <button type="button">جزییات</button>
+                <button type="button">حذف</button>
+                <button type="button">ویرایش</button>
               </td>
             </tr>
-            <tr>
+            <tr className="child:w-56 child:text-lg">
               <td>
-                <img src="../image/head.jpeg"></img>
+                <img src="../image/head.jpeg" className="size-64" />
               </td>
               <td>هنذفری بلوتوثی</td>
               <td>200000 تومان</td>
               <td>134</td>
-              <td>
-                <button>جزِییات</button>
-                <button>حذف</button>
-                <button>ویرایش</button>
+              <td className="child:ml-5 child:bg-blue-700 child:w-20 child:h-14 child:rounded-2xl child:text-white">
+                <button type="button">جزییات</button>
+                <button type="button">حذف</button>
+                <button type="button">ویرایش</button>
               </td>
             </tr>
-            <tr>
+            <tr className="child:w-56 child:text-lg">
               <td>
-                <img src="../image/charger.jpeg"></img>
+                <img src="../image/charger.jpeg" className="size-64" />
               </td>
               <td>هنذفری بلوتوثی</td>
               <td>200000 تومان</td>
               <td>134</td>
-              <td>
-                <button>جزِییات</button>
-                <button>حذف</button>
-                <button>ویرایش</button>
+              <td className="child:ml-5 child:bg-blue-700 child:w-20 child:h-14 child:rounded-2xl child:text-white">
+                <button type="button">جزییات</button>
+                <button type="button">حذف</button>
+                <button type="button">ویرایش</button>
               </td>
             </tr>
           </tbody>
