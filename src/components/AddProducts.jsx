3c83ecd8753b87b202b256b47f528b0cc116905b
impooya <samarkarpooya@gmail.com>
2024-09-03 00:36:41 +0330

    add test add products

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -5,13 +5,66 @@ import { FaRegImage } from "react-icons/fa";
 import { AiOutlineLike } from "react-icons/ai";
 import { IoColorPaletteOutline } from "react-icons/io5";
 import { IoAnalyticsSharp } from "react-icons/io5";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function AddProducts() {
+  // State for new product details
+  const [titleProduct, setTitleProduct] = useState("");
+  const [priceProduct, setPriceProduct] = useState("");
+  const [countProduct, setCountProduct] = useState("");
+  const [imgProduct, setImgProduct] = useState("");
+  const [popularityProduct, setPopularityProduct] = useState("");
+  const [saleProduct, setSaleProduct] = useState("");
+  const [colorsProduct, setColorsProduct] = useState("");
+  const newInfoProducts = {
+    title: titleProduct,
+    price: priceProduct,
+    count: countProduct,
+    img: imgProduct,
+    popularity: popularityProduct,
+    sale: saleProduct,
+    colors: colorsProduct,
+  };
+  const queryClient = useQueryClient();
+  const { mutate: addProduct } = useMutation({
+    mutationFn: (newInfoProducts) => {
+      return axios.post("http://localhost:3000/api/products/", newInfoProducts);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["products"]);
+    },
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+
+  function addNewProductHandler(e) {
+    if (
+      titleProduct &&
+      priceProduct &&
+      countProduct &&
+      imgProduct &&
+      popularityProduct &&
+      saleProduct &&
+      colorsProduct
+    ) {
+      addProduct(newInfoProducts);
+    } else {
+      alert("مفادیر مورد نیاز وارد کنید");
+    }
+    e.preventDefault();
+  }
+
   return (
     <>
       <section className="flex flex-col justify-start items-start gap-y-6">
         <h1 className="text-3xl">افزودن محصول جدید</h1>
-        <form className="w-full bg-white p-9 rounded-2xl shadow-2xl">
+        <form
+          className="w-full bg-white p-9 rounded-2xl shadow-2xl"
+          onSubmit={(event) => addNewProductHandler(event)}
+        >
           <section className="w-full flex flex-col items-end gap-y-4">
             <div className="flex w-full justify-start  items-center  gap-x-4">
               <span className="flex  justify-center items-center gap-3 h-14 w-1/2 text-2xl bg-zinc-400/50 rounded-2xl pr-5">
@@ -19,6 +72,10 @@ export default function AddProducts() {
                 <input
                   className="w-full outline-none border-none bg-transparent placeholder:text-zinc-600"
                   type="text"
+                  value={titleProduct}
+                  onChange={(e) => {
+                    setTitleProduct(e.target.value);
+                  }}
                   placeholder={`اسم محصول را وارد کنید`}
                 />
               </span>
@@ -27,6 +84,10 @@ export default function AddProducts() {
                 <input
                   className="w-full outline-none border-none bg-transparent placeholder:text-zinc-600"
                   type="text"
+                  value={priceProduct}
+                  onChange={(e) => {
+                    setPriceProduct(e.target.value);
+                  }}
                   placeholder={`قیمت محصول را وارد کنید`}
                 />
               </span>
@@ -37,6 +98,10 @@ export default function AddProducts() {
                 <input
                   className="w-full outline-none border-none bg-transparent placeholder:text-zinc-600"
                   type="text"
+                  value={countProduct}
+                  onChange={(e) => {
+                    setCountProduct(e.target.value);
+                  }}
                   placeholder={`موجودی محصول را بنویسید`}
                 />
               </span>
@@ -45,6 +110,10 @@ export default function AddProducts() {
                 <input
                   className="w-full outline-none border-none bg-transparent placeholder:text-zinc-600"
                   type="text"
+                  value={imgProduct}
+                  onChange={(e) => {
+                    setImgProduct(e.target.value);
+                  }}
                   placeholder={`ادرس عکس محصول را بنویسید`}
                 />
               </span>
@@ -55,6 +124,10 @@ export default function AddProducts() {
                 <input
                   className="w-full outline-none border-none bg-transparent placeholder:text-zinc-600"
                   type="text"
+                  value={popularityProduct}
+                  onChange={(e) => {
+                    setPopularityProduct(e.target.value);
+                  }}
                   placeholder={`میزان محبوبیت محصول را بنویسید`}
                 />
               </span>
@@ -63,6 +136,10 @@ export default function AddProducts() {
                 <input
                   className="w-full outline-none border-none bg-transparent placeholder:text-zinc-600"
                   type="text"
+                  value={saleProduct}
+                  onChange={(e) => {
+                    setSaleProduct(e.target.value);
+                  }}
                   placeholder={`میزان فروش محصول را بنویسید`}
                 />
               </span>
@@ -73,6 +150,10 @@ export default function AddProducts() {
                 <input
                   className="w-full outline-none border-none bg-transparent placeholder:text-zinc-600"
                   type="text"
+                  value={colorsProduct}
+                  onChange={(e) => {
+                    setColorsProduct(e.target.value);
+                  }}
                   placeholder={`تعداد رنگ بندی محصول را بنویسید`}
                 />
               </span>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import "./index.css";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
