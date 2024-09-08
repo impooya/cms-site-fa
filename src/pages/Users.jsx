474709ef3295b217e345c0d759f7089ed21cee0f
impooya <samarkarpooya@gmail.com>
2024-09-08 14:30:40 +0330

    add first temp of users table

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,6 +3,31 @@ import ErorrMessage from "../components/ErorrMessage";
 export default function Users() {
   return (
     <>
+      <section className="w-full bg-white mt-4 rounded-2xl shadow-2xl pt-4 ">
+        <thead className="h-20 text-center text-2xl">
+          <tr>
+            <th>نام و نام خانوادگی</th>
+            <th>یوزرنیم</th>
+            <th>رمز عبور</th>
+            <th>شماره تماس</th>
+            <th>ایمیل</th>
+          </tr>
+        </thead>
+        <tbody className="text-center divide-y-2">
+          <tr className="child:w-72 child:text-xl">
+            <td>{"حسینی نژاد"}</td>
+            <td>{"ali_hsyni"}</td>
+            <td>{"ali_9991"}</td>
+            <td>{"9158798712"}</td>
+            <td>{"ali@gmail.com"}</td>
+            <td className="child:ml-5 child:bg-blue-700 child:w-20 child:h-14 child:rounded-2xl child:text-white space-y-5">
+              <button type="button">جزییات</button>
+              <button type="button">حذف</button>
+              <button type="button">ویرایش</button>
+            </td>
+          </tr>
+        </tbody>
+      </section>
       <ErorrMessage msg={"هیچ کاربری پیدا نشد"} />
     </>
   );
